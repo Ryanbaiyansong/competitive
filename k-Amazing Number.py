db4edd3b613be20565a92da2ad61566daca47ff2
Yansong Bai <43448174+Ryanbaiyansong@users.noreply.github.com>
2021-01-01 15:41:56 -0500

    k-Amazing Number

---
+++ k-Amazing Number.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    n = int(input());a = [int(x) - 1 for x in input().split()];prev = [-1 for _ in range(n)];val = [1 for _ in range(n)];ans = [-1 for _ in range(n + 1)];r = n + 1
+    for i, x in enumerate(a):delta = i - prev[x];val[x] = max(val[x], delta);prev[x] = i
+    for i in range(n):val[i] = max(val[i], n - prev[i])    
+    for i in range(n):
+        if val[i] < r:
+            for j in range(val[i], r):ans[j] = i + 1
+            r = val[i]
+    print(' '.join([str(x) for x in ans[1:]]))
