69caa38de7f3bc8a50a171fdfb6d28c5f3909abf
Yansong Bai <43448174+Ryanbaiyansong@users.noreply.github.com>
2021-01-03 11:42:44 -0500

    Perform Easily

---
+++ Perform Easily.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int a[10];
+int b[100005];
+int ans=0x3f3f3f3f;
+int main(){
+	int n,i,j,k;
+	for(i=0;i<6;i++) cin>>a[i];
+	sort(a,a+6);
+	a[6]=1e9;
+	cin>>n;
+	for(i=0;i<n;i++) cin>>b[i];
+	sort(b,b+n);
+	for(i=0;i<n;i++){
+		for(j=0;j<6;j++){
+			int l=b[i]-a[j],r=0;
+			if(l>b[0]-a[0]) continue;
+			for(k=0;k<6;k++) r=max(r,*(lower_bound(b+1,b+n,l+a[k+1])-1)-a[k]);
+			ans=min(ans,r-l);
+		}
+	}
+	cout<<ans;
+}
